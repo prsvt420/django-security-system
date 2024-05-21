a18c544e9284d31f95da5eb9a9697f745d0702c9
Denis Tokarev <wf.denis.tokarev@gmail.com>
2024-05-21 15:42:09 +0800

    Added indexes to models

--- security_system/models.py
+++ security_system/models.py
@@ -11,7 +11,7 @@ class BlacklistUser(models.Model):
         verbose_name = 'Blacklist user'
         verbose_name_plural = 'Blacklist users'
 
-    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE, db_index=True)
     reason = models.TextField()
     start_of_blocking = models.DateTimeField(auto_now_add=True)
     end_of_blocking = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=365 * 30))
@@ -25,7 +25,7 @@ class BlacklistIPAddress(models.Model):
         verbose_name = 'Blacklist ip address'
         verbose_name_plural = 'Blacklist ip addresses'
 
-    ip_address = models.GenericIPAddressField()
+    ip_address = models.GenericIPAddressField(db_index=True)
     reason = models.TextField()
     start_of_blocking = models.DateTimeField(auto_now_add=True)
     end_of_blocking = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=365 * 30))
